07b1f1c9f928b86bc310b2787eecd5d3a947fe15
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-18 11:04:03 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Nested ro‘yxatlar: `[[10,20,30], [40,50], [60,70,80,90]]`. Har bir ichki ro‘yxatning minimal elementini toping.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
